c385eed91229841dc1715ad33c9565a3de0d9b84
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-12 23:38:25 +0900

    ADD 로그인기능

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./componenets/LoginForm";
+import "./App.css";
 
 function App() {
+  const adminUser = {
+    email: "admin@ssafy.com",
+    password: "ssafy",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("로그인되었습니다.");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("로그인에 실패하였습니다");
+      setError("로그인에 실패하였습니다");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
