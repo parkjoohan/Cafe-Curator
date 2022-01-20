6baf7213d53b660528d7cf74e1ad3fbb4738ca32
이승현 <nwlee7950@naver.com>
2022-01-20 13:37:07 +0900

    add: Token 객체

---
+++ backend/src/main/java/com/kql/caffein/dto/Token.java
@@ -0,0 +1,15 @@
+package com.kql.caffein.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+    String accessToken;
+    String refreshToken;
+}
