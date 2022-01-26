a27a64f599e087646ec1bf67789b2391da2062b0
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-27 02:43:40 +0900

    ADD MainFeed2, Like && FIX MainFeed, Routers, userProfile
    Former-commit-id: 99d36458879454498ada3f5b6aed0505edeac98d

--- src/components/user/userProfile.js
+++ src/components/user/userProfile.js
@@ -7,45 +7,6 @@ import EmailForm from "./EmailForm";
 
 export const UserProfile = () => {
     
-    const [currentClick, setCurrentClick] = React.useState(0);
-    const [ViewContent, setViewContent] = React.useState(true);
-
-    const GetClick = (e) => {
-      if (currentClick !== e){
-      setCurrentClick(e);
-      console.log(e);
-      }
-    };
-  
-    
-    React.useEffect(
-        () => {
-          if (currentClick === 0) {
-            let current = document.getElementById('btnplus');
-            let previous = document.getElementById('case2');
-            console.log(current);
-            current.style.color = "white";
-            current.style.backgroundColor = "#484848";
-            previous.style.color = "black";
-            previous.style.backgroundColor = "white";
-            // ViewContent = 'true'
-          }
-
-          else if (currentClick === 1) {
-            let current = document.getElementById('case2');
-            let previous = document.getElementById('btnplus');
-            console.log(current);
-            current.style.color = "white";
-            current.style.backgroundColor = '#484848';
-            previous.style.color = "black";
-            previous.style.backgroundColor = "white";
-            // ViewContent = 'false'
-            
-          }
-        },
-        [currentClick]
-        
-      );
     // const[feed, setfeed] = useState(true)
     // const[follow, setfollow] = useState(false)
 
@@ -58,7 +19,6 @@ export const UserProfile = () => {
     //   document.getElementById('follow').style.backgroundColor = 'black'
     //   document.getElementById('follow').style.color = 'white'
     // }
-  
   
   return (
     <div>
@@ -74,24 +34,13 @@ export const UserProfile = () => {
         
       </Container>
       <hr id="HrStyle"></hr>
-      <div id="ButtonDiv" class="btn-group" role="group" aria-label="Basic example">
-        <button onClick={() => {GetClick(0);setViewContent(true)}} type="button" id="btnplus" class="btn" >
-          
-          <i class="fas fa-anchor fa-sm pr-2"
-            aria-hidden="true"></i> Feed</button>
-        <button onClick={() => {GetClick(1);setViewContent(false)}} type="button" id="case2" class="btn"><i class="fas fa-user-secret fa-sm pr-2"
-            aria-hidden="true"></i> Bookmark</button>
-      </div>
+
       {/* <Container id="ContainerWidth">
         <Row>
           <Col id="ProfileCol" sm={6}><Button id="ProfileButtonLeft">One</Button></Col>
           <Col id="ProfileCol" sm={6}><Button id="ProfileButton">Two</Button></Col>
         </Row>
       </Container> */}
-      <div>
-        { ViewContent ? "하이" : "이하이" }
-        {/* 여기에 이제 컴포넌트 집어넣을거임! */}
-      </div>
       <Container>
         <Row>
           <Col sm={4}><img name="FeedImage" id ="FeedImage" src={process.env.PUBLIC_URL + "/image/map.png"}></img></Col>
