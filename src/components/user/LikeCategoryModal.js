76636c43c7c09e5dbba94ced16de67c5572610ec
byeongjae <ybj121725@gmail.com>
2022-01-27 03:32:36 +0900

    FIX LikeCategory to 관심사선택구현
    Former-commit-id: 71cb971de8b37f9bc03bf769ade77e009bf9eebe

--- src/components/user/LikeCategoryModal.js
+++ src/components/user/LikeCategoryModal.js
@@ -3,6 +3,8 @@ import  { Modal, Button, Col, Row } from 'react-bootstrap';
 import './css/LikeCategory.css'
 
 const LikeCategoryModal = ({ show, onHide }) => {
+
+  const [cnt,setCnt] = useState(0);
   
   const [like,setlike] = useState([
     [0,false],
@@ -11,18 +13,25 @@ const LikeCategoryModal = ({ show, onHide }) => {
     [3,false],
   ])
 
-  let likename = ['관심사1','관심사2','관심사3','관심사4'];
+  let likename = ['메뉴관심사1','메뉴관심사2','메뉴관심사3','메뉴관심사4'];
 
   const selectlike = (n) => {
+    if(cnt==3){
+      alert('관심사는 최대 3개까지 선택 가능합니다.')
+    }else{
     const newlike = [...like];
     newlike[n][1] = true;
     setlike(newlike);
+    setCnt(prev=>prev+1);
+    }
   }
+  
 
   const unlike = (n) => {
     const newlike = [...like];
     newlike[n][1] = false;
     setlike(newlike);
+    setCnt(prev=>prev-1);
   }
 
   const defaultlike = like.map((num)=>(
@@ -57,6 +66,67 @@ const LikeCategoryModal = ({ show, onHide }) => {
     </>
   ))
 
+  // 여기까지는 메뉴관심사코드
+
+  const [like2,setlike2] = useState([
+    [0,false],
+    [1,false],
+    [2,false],
+    [3,false],
+  ])
+
+  let likename2 = ['분위기관심사1','분위기관심사2','분위기관심사3','분위기관심사4'];
+
+  const selectlike2 = (n) => {
+    if(cnt==3){
+      alert('관심사는 최대 3개까지 선택 가능합니다.')
+    }else{
+    const newlike = [...like2];
+    newlike[n][1] = true;
+    setlike2(newlike);
+    setCnt(prev=>prev+1);
+    }
+  }
+
+  const unlike2 = (n) => {
+    const newlike = [...like2];
+    newlike[n][1] = false;
+    setlike2(newlike);
+    setCnt(prev=>prev-1);
+  }
+
+  const defaultlike2 = like2.map((num)=>(
+    <>
+      {num[1]==false?
+        <Col lg={3} md={5} xs={10}>
+          <div 
+          className='likebutton' 
+          id={num[0]}
+          onClick={()=>selectlike2(num[0])}
+          >{likename2[num[0]]}</div>
+        </Col>
+        :
+        <></>
+      }
+    </>
+  ))
+
+  const selected2 = like2.map((num)=>(
+    <>
+      {num[1]==true?
+        <Col xs={2}>
+          <div 
+          className='selected' 
+          id={num[0]}
+          onClick={()=>unlike2(num[0])}
+          >{likename2[num[0]]}</div>
+        </Col>
+        :
+        <></>
+      }
+    </>
+  ))
+
   return (
     <Modal
       show={show}
@@ -79,12 +149,12 @@ const LikeCategoryModal = ({ show, onHide }) => {
           </Col>
         </Row>
         
-        {/* 선택된 카테고리 */}선택된 카테고리
+        {/* 선택된 카테고리 */}선택된 카테고리{selected}{selected2}
         <hr />
 
-        {/* 분위기 카테고리 */}분위기 카테고리<br /><br /><br />
+        {/* 분위기 카테고리 */}분위기 카테고리{defaultlike2}<br /><br /><br />
 
-        {/* 메뉴 카테고리 */}메뉴 카테고리<br /><br /><br />
+        {/* 메뉴 카테고리 */}메뉴 카테고리{defaultlike}<br /><br /><br />
         
       </Modal.Body>
       <Modal.Footer style={{ height: "50px", alignContent: "center" }}>
