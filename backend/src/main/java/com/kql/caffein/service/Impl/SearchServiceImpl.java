244a947de3926589538550c60d5e2b51db90bc66
sohee960328 <sohee960328@gmail.com>
2022-02-03 16:30:03 +0900

    add 카페 검색
    Former-commit-id: e62a89ec6df5095014cc80d2049c8f09b407ff20

--- backend/src/main/java/com/kql/caffein/service/Impl/SearchServiceImpl.java
+++ backend/src/main/java/com/kql/caffein/service/Impl/SearchServiceImpl.java
@@ -1,28 +1,36 @@
 package com.kql.caffein.service.Impl;
 
+import com.kql.caffein.dto.CafeSearchDto;
+import com.kql.caffein.dto.Feed.FeedResDto;
 import com.kql.caffein.entity.Feed.Feed;
+import com.kql.caffein.repository.CafeRepository;
 import com.kql.caffein.repository.FeedRepository;
 import com.kql.caffein.service.FeedService;
 import com.kql.caffein.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class SearchServiceImpl implements SearchService {
 
     private final FeedRepository feedRepository;
     private final FeedService feedService;
+    private final CafeRepository cafeRepository;
+    private final RedisTemplate redisTemplate;
 
     @Autowired
-    public SearchServiceImpl(FeedRepository feedRepository, FeedService feedService) {
+    public SearchServiceImpl(FeedRepository feedRepository, FeedService feedService, CafeRepository cafeRepository, RedisTemplate redisTemplate) {
         this.feedRepository = feedRepository;
         this.feedService = feedService;
+        this.cafeRepository = cafeRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -46,5 +54,26 @@ public class SearchServiceImpl implements SearchService {
         Page<Feed> feedList = feedRepository.findByCategoryListOrderByRegTime(lastFeedNo, category, pageRequest);
 
         return feedService.makeFeedDtoList(feedList.getContent(), userNo);
+    }
+
+    @Override
+    public CafeSearchDto cafeSearchWithPaging(String userNo, String cafeName, Integer lastFeedNo, int size) {
+        int cafeId = cafeRepository.findByCafeName(cafeName).get().getCafeId();
+        Set<String> category = redisTemplate.opsForZSet().reverseRange(String.valueOf(cafeId),0,1);
+
+        if(lastFeedNo == null) lastFeedNo = Integer.MAX_VALUE;
+        PageRequest pageRequest  = PageRequest.of(0, size);
+        Page<Feed> feedList = feedRepository.findByCafeIdAndFeedNoLessThanOrderByFeedNoDesc(cafeId, lastFeedNo, pageRequest);
+
+        for(Feed f : feedList){
+            System.out.println(f.toString());
+        }
+
+        List<FeedResDto> feedResDtoList = feedService.makeFeedDtoList(feedList.getContent(), userNo);
+
+        CafeSearchDto cafeSearchDto = CafeSearchDto.builder()
+                .categoryList(category)
+                .feedList(feedResDtoList).build();
+        return cafeSearchDto;
     }
 }
--- backend/src/main/java/com/kql/caffein/service/SearchService.java
+++ backend/src/main/java/com/kql/caffein/service/SearchService.java
@@ -1,4 +1,6 @@
 package com.kql.caffein.service;
+import com.kql.caffein.dto.CafeSearchDto;
+
 import java.util.List;
 
 public interface SearchService {
@@ -7,4 +9,7 @@ public interface SearchService {
     
     //카테고리 검색 - 최신순
     List categorySearchRecentWithPaging(String userNo, String category, Integer lastFeedNo, int size);
+    
+    //카페 검색
+    CafeSearchDto cafeSearchWithPaging(String userNo, String cafeName, Integer lastFeedNo, int size);
 }
