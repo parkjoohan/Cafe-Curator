5ff8e9b007df0f547e4772b1214253af1a035096
sohee960328 <sohee960328@gmail.com>
2022-02-06 21:01:49 +0900

    add spring cors 설정
    Former-commit-id: 5cf2693ff7d3ebdcb2ebaba411f8b3ad381aca6a

---
+++ backend/src/main/java/com/kql/caffein/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.kql.caffein.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("X-AUTH-TOKEN")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000");
+    }
+}
