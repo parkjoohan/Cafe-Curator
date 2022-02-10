78bd60d338ba029be73a55e995ac778ba3fea2c9
byeongjae <ybj121725@gmail.com>
2022-02-10 18:41:50 +0900

    FIX  locationsearch 지도완성
    Former-commit-id: 5e3a9234f132c8955a072928ab281b8390cdf18d

--- frontend/src/components/feed/search/LocationSearch.js
+++ frontend/src/components/feed/search/LocationSearch.js
@@ -6,16 +6,20 @@ import Search from './Search';
 import Map from './Map';
 
 export default function LocationSearch(props) {
+    const mapRef = useRef();
     const [data,setData] = useState([]);
 
     useEffect(()=>{
-        console.log(data)
+        if(mapRef.current){
+            mapRef.current.setMarkerdata(data)
+        }
     },[data])
+
     return (
         <Container>
             <Search setData={setData}/>
             <div>
-                <Map id="LocationSearch_map" data={data}/>
+                <Map id="LocationSearch_map" ref={mapRef}/>
             </div>
             <div id='LocationSearch_blog'>
                 <Blog />
