c04c94891e26a6c0fcd55cb8cd325f5fc29eab9e
sohee960328 <sohee960328@gmail.com>
2022-02-11 21:02:42 +0900

    fix WebMvcConfig
    Former-commit-id: 46be112950a1c8bd832c5b8ace2d38e6ea86f29b

--- backend/src/main/java/com/kql/caffein/config/WebMvcConfig.java
+++ backend/src/main/java/com/kql/caffein/config/WebMvcConfig.java
@@ -1,15 +1,15 @@
 package com.kql.caffein.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
                 .allowedOrigins("http://localhost:3000", "http://i6c104.p.ssafy.io:3000");
     }
 }
