23b0d0a003c49c9193d18faf683812028e8219d3
byeongjae <ybj121725@gmail.com>
2022-02-15 09:48:49 +0900

    FIX home
    Former-commit-id: d5233d380e25f450b6dfc7efa6c7fea4af24e79f

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -4,59 +4,77 @@ import './css/Home.css'
 import styles from './css/Home.module.css'
 import Card from './Card';
 import { Link } from 'react-router-dom'
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 import axios from 'axios';
 
 export default function Home(props) {
 
+  useEffect(()=>{
+    AOS.init();
+  },[])
+
   return (
 
     <div id="home_home">
       <div id='home_section1'>
-        <div id='hone_video_bg'>
+        {/* <div id='hone_video_bg'> */}
           <video id='home_video' src='../image/black_cafe.mp4' autoPlay loop muted></video>
           <div id='home_text_wrapper'>
             <h1 id='home_hometitle'>나만의 카페를 찾아보세요!</h1>
             <h3 id='home_introduce'>
-              카페 큐레이팅 서비스 입니다.<br/>
               자신의 관심사를 선택하세요!<br/>
               {props.user}님을 위한 카페를 추천해드립니다!<br/>
-              항상 환영합니다.
             </h3>
           </div>
-        </div>
+        {/* </div> */}
       </div>               
       <div className='section2'>
-        <Container className='content'>
-          <Card
-            title="지도중심 빠른찾기"
-            sub_title="현 위치를 기준으로 카페를 추천"
-            images="../image/Map2.png"
-            alt="map"
-            src="feed"
-          ></Card>&nbsp;&nbsp;
-          <Card
-            title="원두족을 위한 추천"
-            sub_title="커피 맛 좋은 카페를 추천"
-            images="../image/Coffee.png"
-            alt="coffee"
-            src="bookmark"
-          />&nbsp;&nbsp;
-          <Card
-            title="뷰 좋은 카페를 추천"
-            sub_title="전망 좋은 카페를 추천"
-            images="../image/View.png"
-            alt="view"
-            src="login"
-          />&nbsp;&nbsp;
-          <Card
-            title="카공족을 위한 추천"
-            sub_title="공부하기 좋은 카페를 추천"
-            images="../image/Study.png"
-            alt="study"
-            src="signup"
-          />
-        </Container>
+
+        <section class="content-section" data-aos="fade-right" data-aos-duration="2000">
+          <div class="figure">
+            <img src={process.env.PUBLIC_URL + "/image/homeimage/mapsearch.jpg"} />
+            <div class="content">
+              <h1 class="subheading">지도 중심 찾기</h1>
+              <h2 class="heading">내 위치를 중심으로 찾아보세요.</h2>
+              <h2 class="subheading link">더 알아보기 ></h2>
+            </div>
+          </div>
+        </section>
+
+        <section class="content-section" data-aos="fade-right" data-aos-duration="2000">
+          <div class="figure">
+            <img src={process.env.PUBLIC_URL + "/image/homeimage/coffeesearch.jpg"} />
+            <div class="content">
+              <h1 class="subheading">원두 중심 찾기</h1>
+              <h2 class="heading">원두를 중심으로 찾아보세요.</h2>
+              <h2 class="subheading link">더 알아보기 ></h2>
+            </div>
+          </div>
+        </section>
+
+        <section class="content-section" data-aos="fade-right" data-aos-duration="2000">
+          <div class="figure">
+            <img src={process.env.PUBLIC_URL + "/image/homeimage/landscapesearch2.jpg"} />
+            <div class="content">
+              <h1 class="subheading">전망 중심 찾기</h1>
+              <h2 class="heading">전망 좋은 카페를 찾아보세요.</h2>
+              <h2 class="subheading link">더 알아보기 ></h2>
+            </div>
+          </div>
+        </section>
+
+        <section class="content-section" data-aos="fade-right" data-aos-duration="2000">
+          <div class="figure">
+            <img src={process.env.PUBLIC_URL + "/image/homeimage/studysearch.jpg"} />
+            <div class="content">
+              <h1 class="subheading">스터디 카페 찾기</h1>
+              <h2 class="heading">공부하기 좋은 카페를 찾아보세요.</h2>
+              <h2 class="subheading link">더 알아보기 ></h2>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   )
--- frontend/src/components/feed/search/AccountSearch/AccountSearch.js
+++ frontend/src/components/feed/search/AccountSearch/AccountSearch.js
@@ -1,21 +1,44 @@
 import React,{useState,useEffect} from 'react';
 import './css/Account.css';
 import Search from './Search';
+import axios from 'axios'
 
 export default function AccountSearch() {
     const [keyword,setKeyword] = useState("")
+    const [data,setData] = useState([])
     useEffect(()=>{
+        setData([])
         if (keyword.length==0){
             console.log('아직 입력 안함.')
-        }else if(keyword.length<4){
-            console.log('4글자 이하입니다.')
+        }else if(keyword.length<1){
+            alert('4글자 이상 입력하세요!')
         }else{
-            console.log('4글자 이상입니다.')
+            const url = `http://i6c104.p.ssafy.io:8080/search/${keyword}`
+            axios.get(url).then(res=>{
+                setData(res.data);
+                console.log(res.data)
+            })
         }
     },[keyword])
     return (
         <div>
             <Search setKeyword={setKeyword}/>
+            {
+                data.map((account,index)=>(
+                    <div style={{display:"flex",justifyContent:"center"}}>
+                        <hr/>
+                        {
+                            account.picture?
+                            <img src={account.picture}></img>:
+                            <img id ="fix_profile_pic" src={process.env.PUBLIC_URL + "/image/map.png"}></img>
+                        }
+                        <p>{account.userId}</p>
+                        <p>팔로워{account.followerCount}</p>
+                        <p>팔로잉{account.followingCount}</p>
+                        <hr/>
+                    </div>
+                ))
+            }
         </div>
     );
 }
